d1e8e839f9969cb06d51ee624c439bdbb27e3faf
alabi olanike Temitayo <olanikealabi@rocketmail.com>
2018-04-13 12:08:05 +0100

    Adding stage 1 files to my repo.

---
+++ hng.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HNG Internship</title>
+	<link rel="stylesheet" type="text/css" href="CSS/css.css">
+</head>
+<body>
+<div>
+	<div id='bar'>
+	   <h1 margin='left'><strong>HNG INTERNSHIP 2018</strong></h1>
+    </div>
+
+  <div>
+     <h2>
+     <?php $date= date('Y-m-d H:i:s');
+
+     echo $date; ?>
+     	
+     </h2>
+  </div>
+
+
+
+
+</div>
+</body>
+</html>
